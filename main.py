6967de68d484ee428609f091af077af94a1cb5c1
Travis Mitchum <travism2006@gmail.com>
2021-08-10 16:24:11 -0400

    Added params for class.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 
 class AbstractDecorator:
     """
-
+        Any decorator wraps a function but their are common cases and
+        consistent features/data and information that all decorators share.
     """
-
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.decName = ""
+        self.decArgs = ""
+        self.decKwargs = ""
