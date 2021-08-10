5782300b399f587e3fbf6948c65241b6e1d7e52b
Travis Mitchum <travism2006@gmail.com>
2021-08-10 14:42:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Program to help devs save time making their own decorators
+# Typically in Python decorators may be used to wrap and apply to any functions
+
+class AbstractDecorator:
+    """
+
+    """
+
+    
