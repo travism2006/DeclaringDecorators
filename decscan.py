e6512126137daf31f19e4e422c89403338c8a858
Travis Mitchum <travism2006@gmail.com>
2021-08-18 21:08:08 -0400

    Updated decscan file with some logic updates and added in some exception handling/catching for "with" not working. Still is not finalized yet.  Will need to continue work.

--- decscan.py
+++ decscan.py
@@ -1,27 +1,45 @@
 from pprint import pprint
 # Purpose:
-# This part of the module will enable scanning for decorators
+# This part of the module will enable scanning for decorators and properties
 
-def decScanFile(path: str):
+def decScanFile(someFilePath: str):
     """
     Scans a given python file and returns a list of data
 
-    @type path: r-string path for locating decorators within
+    @type someFilePath: r-string path for locating decorators and properties within
     @return dataList: list containing number of decorators found, frequency of each decorator
     """
+
+    if not someFilePath.endswith(".py"):
+        raise ValueError("File must be a python file to scan for decorators and properties")
+
     fileLineData = []
+    dataDetails = []
     try:
-        with open(path, 'r') as pyFile:
+        with open(someFilePath, 'r') as pyFile:
             # if line in pyFile starts with "@" it's a decorator
-            for line in pyFile.readlines():
-                print(line)
-                fileLineData.append(line)
+            for lineNumb, lineData in enumerate(pyFile.readlines()):
+                # print(line)
+                if "@" in lineData:
+                    fileLineData.append(lineData)
+                    dataDetails.append(f"line: {lineNumb}, line data: {repr(lineData)}")
+    except IOError:
+        print("File was not readable. Check file permissions.")
     except ValueError:
-        print("path value is not of type string. Could not open file.")
+        print("someFilePath value is not of type string. Could not open file.")
+    except FileNotFoundError:
+        print("File was not located.")
     finally:
-        return fileLineData
+        return dataDetails
 
 
 ansList = decScanFile(r"G:\Python Coding\AutoCreateVirtualEnv.py")
 
 pprint(ansList)
+
+try:
+    ansList = decScanFile(r"G:\Python Coding")
+except ValueError:
+    print("Caught value is not a python file.")
+finally:
+    del ansList
